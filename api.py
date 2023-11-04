47c1c3f6dc1b3a9fec9e4e9b9595b4b778ea1612
Subarna Saikia <saikiasubarna74@gmail.com>
2023-11-04 23:12:37 +0530

    created app

--- api.py
+++ api.py
@@ -3,5 +3,5 @@
 def getKey():
     """ OpenAI API key """
     # API_KEY = getpass("Enter your OpenAI API key: ")
-    API_KEY = ""
+    API_KEY = "Enter your own OpenAI API here..."
     return API_KEY
--- app.py
+++ app.py
@@ -1,9 +1,34 @@
-# import os
-# from embedchain import App
+import os
+from embedchain import App
 from api import getKey
 import streamlit as st
 
 
-st.title("Resume Maker Bot...") 
+
 api_key = getKey()
-st.write(f"your api key is {api_key}")
+# if api_key != "":
+os.environ['OPENAI_API_KEY'] = api_key
+app = App()
+
+
+st.title("Resume Maker Bot...") 
+
+
+# linkedIn_link = st.text_input("Enter your LinkedIn profile URL: ")
+# linkedIn_link = 'Enter your linkedIn profile  URL'
+# app.add(linkedIn_link, data_type='web_page')
+
+app.add('./data/Profile_Subarna.pdf', data_type='pdf_file')
+
+
+
+prompt =st.text_input("Enter your prompt....")
+
+Context = f"""
+Hi, Act as a resume builder and based on data available create a resume for software engineer
+{prompt}
+"""
+
+if prompt:
+    response = app.query(Context)
+    st.write(response)
